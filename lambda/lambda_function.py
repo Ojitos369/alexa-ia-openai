70d232ca8b2449f7c3814b80af19c26d43785386
ojitos369 <ojitos369@gmail.com>
2025-01-17 12:46:21 -0600

    [CHANGE] consulta to api sa

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,5 +1,5 @@
 import os
-import openai
+import requests
 import logging
 from dotenv import load_dotenv
 from ask_sdk_core.skill_builder import SkillBuilder
@@ -14,30 +14,17 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 # Clave de OpenAI
-OPEN_AI_API = os.getenv("OPEN_AI_API")
-print("OPEN_AI_API", OPEN_AI_API)
-openai.api_key = OPEN_AI_API
+API_KEY_ACCESS = os.getenv("API_KEY_ACCESS")
 
-def obtener_respuesta_openai(messages: list, model="basico") -> str:
-    """
-    Llama a la API de OpenAI usando ChatCompletion con todo el historial de mensajes.
-    """
-    try:
-        respuesta = openai.ChatCompletion.create(
-            model="gpt-4o" if model == "avanzado" else "gpt-4o-mini",
-            # model="asst_2IHKpjntcemAC95p7ZfbUgyl",
-            messages=messages,
-            max_tokens=1500,
-            temperature=0.7
-        )
-        texto = respuesta.choices[0].message.content.strip()
-        texto = " ".join(texto.split())
-        if not texto:
-            texto = "Lo siento, no tengo una respuesta para eso."
-        return texto
-    except Exception as e:
-        logger.error(f"Error al comunicarse con OpenAI: {e}", exc_info=True)
-        return "Lo siento, no puedo obtener una respuesta en este momento."
+def pregunta_api(message):
+    link = "https://sa.ojitos369.com/api/gpt_con/chat/"
+    data = {
+        "message": message,
+        "key": API_KEY_ACCESS,
+        "origen": "alexa"
+    }
+    response = requests.post(link, data=data)
+    return response.json()["message"]
 
 class LaunchRequestHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
@@ -78,23 +65,8 @@ class OpenAIIntentHandler(AbstractRequestHandler):
 
             logger.info(f"Usuario preguntó: {pregunta_usuario}")
 
-            # Cargar o inicializar el historial de la sesión
-            session_attributes = handler_input.attributes_manager.session_attributes
-            if "messages" not in session_attributes:
-                session_attributes["messages"] = []
-
-            model = "basico"
-            if pregunta_usuario.startswith("avanzado"):
-                model = "avanzado"
-                pregunta_usuario = pregunta_usuario[8:]
-            # Agregar el mensaje del usuario al historial
-            session_attributes["messages"].append({"role": "user", "content": pregunta_usuario})
-
-            # Obtener respuesta de OpenAI con todo el historial
-            respuesta_openai = obtener_respuesta_openai(session_attributes["messages"], model)
-
-            # Agregar el mensaje del asistente al historial
-            session_attributes["messages"].append({"role": "assistant", "content": respuesta_openai})
+            # Obtiene Respuesta
+            respuesta_openai = pregunta_api(pregunta_usuario)
 
             # Seguimos preguntando si tiene otra pregunta, sesión permanece abierta
             return (
